644ae362d0530fbdb82db1f26eed97161fa20592
PBR777 <109592002+PBR777@users.noreply.github.com>
2023-03-01 21:34:40 +0800

    Add files via upload

--- 001.js
+++ 001.js
@@ -3,40 +3,38 @@
 
 
     class PBR {
-      constructor () {}
-  
-      getInfo () {
-        return {
-          id: "pbr",
-          name: "PBR",
-          blocks: [
-            {
-              opcode: "letters_of",
-              blockType: Scratch.BlockType.REPORTER,
-              text: "letters [LETTER1] to [LETTER2] of [STRING]",
-              arguments: {
-                LETTER1: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 2
-                },
-                LETTER2: {
-                  type: Scratch.ArgumentType.NUMBER,
-                  defaultValue: 4
-                },
-                STRING: {
-                  type: Scratch.ArgumentType.STRING,
-                  defaultValue: "apple"
-                }
-              }
-            }
-         ],
-         };
-      }
-  
-      identical(args, util) {
-        return args.OPERAND1 === args.OPERAND2;
-      }
+        constructor() {}
+
+        getInfo() {
+            return {
+                id: "pbr",
+                name: "PBR",
+                blocks: [{
+                    opcode: "letters_of",
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: "letters [LETTER1] to [LETTER2] of [STRING]",
+                    arguments: {
+                        LETTER1: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 2
+                        },
+                        LETTER2: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 4
+                        },
+                        STRING: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "apple"
+                        }
+                    }
+                }],
+            };
+        }
+
+        identical(args, util) {
+            return args.OPERAND1 === args.OPERAND2;
+        }
     }
-  
+
     Scratch.extensions.register(new PBR());
-  })(Scratch);
+})(Scratch);
