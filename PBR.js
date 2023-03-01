d6dd3cd826c766db77d0debf90340c5184883c3c
PBR777 <109592002+PBR777@users.noreply.github.com>
2023-03-01 21:35:49 +0800

    Add files via upload

---
+++ PBR.js
@@ -0,0 +1,40 @@
+(function(Scratch) {
+    "use strict";
+
+
+    class PBR {
+        constructor() {}
+
+        getInfo() {
+            return {
+                id: "pbr",
+                name: "PBR",
+                blocks: [{
+                    opcode: "letters_of",
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: "letters [LETTER1] to [LETTER2] of [STRING]",
+                    arguments: {
+                        LETTER1: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 2
+                        },
+                        LETTER2: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 4
+                        },
+                        STRING: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "apple"
+                        }
+                    }
+                }],
+            };
+        }
+
+        identical(args, util) {
+            return args.OPERAND1 === args.OPERAND2;
+        }
+    }
+
+    Scratch.extensions.register(new PBR());
+})(Scratch);
